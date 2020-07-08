eeea55f06bed4a3852a0b99411ea727d2e38f872
Tadhg Lydon <talydon@gmail.com>
2020-07-08 12:16:45 +0100

    First Commit

---
+++ MaintainanceApi/Controllers/MaintenanceController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ApiTestPad.Controllers
+{
+    [ApiController]
+    [Route("{*url}")]
+    public class MaintenanceController : ControllerBase
+    {
+        private readonly ILogger<MaintenanceController> _logger;
+
+        public MaintenanceController(ILogger<MaintenanceController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return StatusCode(503);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return StatusCode(503);
+        }
+
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return StatusCode(503);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return StatusCode(503);
+        }
+    }
+}
